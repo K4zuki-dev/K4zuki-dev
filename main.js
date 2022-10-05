ff91655db1169c6eba0c0b555e3e80cdaa720ca8
K4zuki-dev <113984501+K4zuki-dev@users.noreply.github.com>
2022-10-05 22:40:24 +0200

    Geschichte website

--- main.js
+++ main.js
@@ -1,23 +1,37 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from "three"
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const  scene = new THREE.Scene();
 
-setupCounter(document.querySelector('#counter'))
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGL1Renderer({
+    canvas: document.getElementById("bg")
+})
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({ color: 0xFF6347});
+const torus = new THREE.Mesh(geometry, material);
+
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(0, -0, -10)
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+
+
+
+scene.add(lightHelper)
+scene.add(pointLight)
+scene.add(torus)
+
+function animate() {
+  torus.rotation.y += 0.01
+
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
+}
+
+animate()
