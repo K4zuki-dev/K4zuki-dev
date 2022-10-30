22c790b4a87fc865244a365bdf81a61bc45efa56
K4zuki-dev <113984501+K4zuki-dev@users.noreply.github.com>
2022-10-30 21:22:22 +0100

    First 3d

--- three.js
+++ three.js
@@ -1,10 +1,11 @@
 import * as THREE from 'https://threejs.org/build/three.module.js'; 
 import { OrbitControls } from './three/examples/jsm/controls/OrbitControls.js';
+import {GLTFLoader} from "./three/examples/jsm/loaders/GLTFLoader"
 
 console.log(THREE, OrbitControls);
 
 
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
 const scene1 = new THREE.Scene();
 const scene2 = new THREE.Scene()
 
@@ -20,20 +21,31 @@ const first = document.querySelector("#first")
 const second = document.querySelector("#second")
 const third = document.querySelector("#third")
 
+var model
+
+const loader = new GLTFLoader();
+
+loader.load("./3Dobj/Reichsbank/Reichsbank.gltf", function(gltf) {
+  model = gltf.scene
+
+  scene.add(gltf.scene)
+  render()
+})
+
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector("#first"),
   antialias: true
 });
 
-const renderer1 = new THREE.WebGLRenderer({
-  canvas: document.querySelector("#second"),
-  antialias: true
-});
+// const renderer1 = new THREE.WebGLRenderer({
+//   canvas: document.querySelector("#second"),
+//   antialias: true
+// });
 
-const renderer2 = new THREE.WebGLRenderer({
-  canvas: document.querySelector("#third"),
-  antialias: true
-})
+// const renderer2 = new THREE.WebGLRenderer({
+//   canvas: document.querySelector("#third"),
+//   antialias: true
+// })
 
 
 function resize() {
@@ -47,54 +59,51 @@ function resize() {
   camera.aspect = width / height;
   camera.updateProjectionMatrix();
 
-  renderer1.setSize(width, height)
-  camera1.aspect = width/height
-  camera1.updateProjectionMatrix()
+  // renderer1.setSize(width, height)
+  // camera1.aspect = width/height
+  // camera1.updateProjectionMatrix()
 
-  renderer2.setSize(width, height)
-  camera2.aspect = width/height
-  camera2.updateProjectionMatrix()
-};
+  // renderer2.setSize(width, height)
+  // camera2.aspect = width/height
+  // camera2.updateProjectionMatrix()
+}
 
 
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
-camera1.position.setZ(30)
-camera2.position.setZ(30)
+// renderer.setPixelRatio(window.devicePixelRatio);
+// renderer.setSize(window.innerWidth, window.innerHeight);
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xFF6347});
-const torus = new THREE.Mesh(geometry, material);
+// Setting Camera position
 
-const geometry1 = new THREE.TorusGeometry(10, 3, 16, 100);
-const material1 = new THREE.MeshStandardMaterial({color: 0xFF6347});
-const torus1 = new THREE.Mesh(geometry1, material1);
+function cameraPos(cam) {
+  cam.position.setZ(30)
+  cam.position.setY(20)
+}
 
-const geometry2 = new THREE.TorusGeometry(10, 3, 16, 100);
-const material2 = new THREE.MeshStandardMaterial({color: 0xFF6347});
-const torus2 = new THREE.Mesh(geometry2, material2);
+cameraPos(camera)
+cameraPos(camera1)
+cameraPos(camera2)
 
 const pointLight = new THREE.PointLight(0xffffff);
 const pointLight1 = new THREE.PointLight(0xffffff);
 const pointLight2 = new THREE.PointLight(0xffffff);
+
+pointLight.position.set(0, 30, 50)
 
 const lightHelper = new THREE.PointLightHelper(pointLight);
 const lightHelper1 = new THREE.PointLightHelper(pointLight1)
 const lightHelper2 = new THREE.PointLightHelper(pointLight2)
 
 const controls = new OrbitControls(camera, renderer.domElement);
-const controls1 = new OrbitControls(camera1, renderer1.domElement)
-const controls2 = new OrbitControls(camera2, renderer2.domElement)
+// const controls1 = new OrbitControls(camera1, renderer1.domElement)
+// const controls2 = new OrbitControls(camera2, renderer2.domElement)
 
-addScene(scene, lightHelper, pointLight, torus)
-addScene(scene1, lightHelper1, pointLight1, torus1)
-addScene(scene2, lightHelper2, pointLight2, torus2)
+addScene(scene, lightHelper, pointLight)
+addScene(scene1, lightHelper1, pointLight1 )
+addScene(scene2, lightHelper2, pointLight2 )
 
-function addScene(scene, obj1, obj2, obj3) {
+function addScene(scene, obj1, obj2) {
   scene.add(obj1)
   scene.add(obj2)
-  scene.add(obj3)
 }
 
 let bool = true
@@ -114,24 +123,24 @@ changeBool(first)
 changeBool(second)
 changeBool(third)
 
-renderer.setClearColor (0x757876, 1)
-renderer1.setClearColor (0x757876, 1)
-renderer2.setClearColor (0x757876, 1)
+// renderer.setClearColor (0x757876, 1)
+// renderer1.setClearColor (0x757876, 1)
+// renderer2.setClearColor (0x757876, 1)
 
-function animate() {
+function render() {
   if (bool) {
-    torus.rotation.y += .005
-    torus1.rotation.y -= .005
-    torus2.rotation.y += .005
+    if(model) {
+      model.rotation.y += 0.001
+    }
   }
 
-  requestAnimationFrame(animate)
+  requestAnimationFrame(render)
   renderer.render(scene, camera)
-  renderer1.render(scene1, camera1)
-  renderer2.render(scene2, camera2)
+  // renderer1.render(scene1, camera1)
+  // renderer2.render(scene2, camera2)
 }
 
-animate()
+render()
 resize()
 
 console.log("test-after-THREE");
