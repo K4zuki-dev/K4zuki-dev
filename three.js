ca98502aa342278e6d3991f46753ece05d90d39b
K4zuki-dev <113984501+K4zuki-dev@users.noreply.github.com>
2022-10-31 18:28:42 +0100

    Update three.js

--- three.js
+++ three.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://threejs.org/build/three.module.js'; 
-import { PointLightHelper } from 'three';
+import { PointLightHelper } from 'https://threejs.org/build/three.module.js';
 import { OrbitControls } from './three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from "./three/examples/jsm/loaders/GLTFLoader.js"
 
